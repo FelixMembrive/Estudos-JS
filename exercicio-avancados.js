802f2292d6b626210abe0c7dc0353539bc9dd3f6
FelixMembrive <felix.membrive@gmail.com>
2022-04-17 23:26:18 -0300

    "Estudos"

--- exercicio-avancados.js
+++ exercicio-avancados.js
@@ -1,32 +1,30 @@
-const calculadora = (entrada, operacao, numero1, numero2) => {
-    const resultado = `${numero1} ${operacao} ${numero2}`
+const calculadora = (entrada, numero1, numero2) => {
+    switch (entrada) {
+        case "Multiplicação":
+            console.log(numero1 * numero2);
+            break;
+        case "Adição":
+            console.log(numero1 + numero2);
+            break;
+        case "Subtração":
+            console.log(numero1 - numero2);
+            break;
+        case "Divisão":
+            console.log(numero1 / numero2);
+            break;
+        default:
+            console.log(
+                "Operação não cadastrada." + "\n" +
+                "Operações Cadastradas: Multiplicação, Adição, Subtração e Divisão"
+            );
+    }
 }
 
-
-const entrada = "Multiplicação"
-
-switch (operacao) {
-    case "Multiplicação":
-        const operacao = "*";
-        break;
-
-    case "Adição":
-        const operacao = "+";
-        break;
-
-    case "Subtração":
-        const operacao = "+";
-        break;
-
-    default:
-        "Operação não cadastrada" + "\n" +
-        "Operações Cadastradas: Multiplicação, Adição, Subtração e Divisão"
-}
-
-console.log(operacao);
+calculadora("grgr", 10, 2)
 
 
-// const expr = 'Papayas';
+const expr = 'Papayas';
+
 // switch (expr) {
 //     case 'Oranges':
 //         'Oranges are $0.59 a pound.'
@@ -38,4 +36,58 @@ console.log(operacao);
 //         break;
 //     default:
 //         console.log(`Sorry, we are out of ${expr}.`);
+// }
+
+
+for (let index = 1; index <= 50; index++) {
+    console.log(index);
+}
+for (let index = 1; index <= 50; index++) {
+    if (index > 25) {
+        break
+    }
+    console.log(index);
+}
+for (let index = 1; index <= 50; index++) {
+    if (index === 10) {
+        continue
+    }
+    console.log(index);
+}
+
+// for (let index = 1; index <= 50; index = index + 10) {
+//     if (index === 60) {
+//         console.log("CONTINUE");
+//         continue
+//     }
+//     if (index === 90) {
+//         console.log("CONTINUE");
+//         continue
+//     }
+//     console.log(index);
+// }
+
+// for (let index = 1; index <= 50; index = index + 10) {
+//     if (index === 60) {
+//         console.log("CONTINUE");
+//         continue
+//     }
+//     if (index === 90) {
+//         console.log("CONTINUE");
+//         continue
+//     }
+//     console.log(index);
+// }
+
+
+// for (let index = 20; index <= 100; index = index + 10) {
+//     if (index === 60) {
+//         console.log("CONTINUE");
+//         continue
+//     }
+//     if (index === 90) {
+//         console.log("CONTINUE");
+//         continue
+//     }
+//     console.log(index);
 // }
